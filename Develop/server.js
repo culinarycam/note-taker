496bddcfdc7a618ded12a564583c0c93681fa9f3
Cameron Singleton <culinarycam98@gmail.com>
2023-09-07 20:18:01 -0400

    added server js

---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const PORT = process.env.PORT ?? 3001;
+const app = express();
+const routes = require('./routes');
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server running @ http://localhost;${PORT}`);
+});
